adc3ec78161e805201fa9b9930b2c8252bba3d17
josuhee <suheeqwe@gmail.com>
2021-06-27 16:08:27 +0900

    손익분기점

---
+++ ps/BJ_1712.c
@@ -0,0 +1,11 @@
+//[백준] 1712 : 손익분기점
+
+#include <stdio.h>
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (b >= c) printf("-1\n");
+    else printf("%d\n", a / (c - b) + 1);
+    return 0;
+}
