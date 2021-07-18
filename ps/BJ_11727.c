72a16cb43cebdbde8d38b2d1d15df4eb21e1f509
josuhee <suheeqwe@gmail.com>
2021-07-18 20:48:05 +0900

    2xn 타일링 2

---
+++ ps/BJ_11727.c
@@ -0,0 +1,24 @@
+//[백준] 11727 : 2xn 타일링 2
+
+#include <stdio.h>
+#define MOD 10007
+
+int dp[1001][2];
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+	dp[1][0] = 1;
+	dp[1][1] = 0;
+	dp[2][0] = 1;
+	dp[2][1] = 2;
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+		dp[i][1] = (dp[i - 2][0] + dp[i - 2][1]) * 2 % MOD;
+	}
+	printf("%d\n", (dp[n][0] + dp[n][1]) % MOD);
+	return (0);
+}
