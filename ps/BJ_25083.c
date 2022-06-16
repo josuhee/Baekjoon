8b02510a63241ce0aa1546774f0a2407762ceeeb
josuhee <suheeqwe@gmail.com>
2022-06-16 12:41:27 +0900

    새싹

---
+++ ps/BJ_25083.c
@@ -0,0 +1,14 @@
+// [백준] 25083 : 새싹
+
+#include <stdio.h>
+
+int main()
+{
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+	return (0);
+}
